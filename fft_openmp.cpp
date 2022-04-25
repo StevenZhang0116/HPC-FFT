fe8fde67118cbac92d779957741e347fe137ec1b
StevenZhang0116 <stevenzhang0116@outlook.com>
2022-04-25 12:01:05 -0400

    test

--- fft_openmp.cpp
+++ fft_openmp.cpp
@@ -9,8 +9,8 @@
 # include <iomanip>
 # include <ctime>
 # include <omp.h>
-using namespace std;
 
+using namespace std;
 
 // step function, use the logic/parameters presented on p151 in Petersen's book
 void step (int n, int mj, double a[], double b[], double c[], double d[], double w[], double sgn){
